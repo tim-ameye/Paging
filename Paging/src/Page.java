a071bbedbaa0f77edbb116f992a191ef4af13647
PieterCarlu <pieter.carlu@telenet.be>
2020-04-21 18:31:31 +0200

    deze zijn denkk in orde

--- Paging/src/Page.java
+++ Paging/src/Page.java
@@ -1,5 +1,51 @@
 
 public class Page {
+	
+	private int frameNumber;
+	private int pID;
+	private int pageNumber;
+	
+	public Page(int pID, int pageNumber) {
+		this.pID = pID;
+		this.pageNumber = pageNumber;
+	}
+	
+	public Page(int pID, int pageNumber, int frameNumber) {
+		this.pID = pID;
+		this.pageNumber = pageNumber;
+		this.frameNumber = frameNumber;
+	}
+	
+	public Page(Page page) {
+		this.pID = page.pID;
+		this.pageNumber = page.pageNumber;
+		this.frameNumber = page.frameNumber;
+	}
 
+	public int getFrameNumber() {
+		return frameNumber;
+	}
+
+	public void setFrameNumber(int frameNumber) {
+		this.frameNumber = frameNumber;
+	}
+
+	public int getpID() {
+		return pID;
+	}
+
+	public void setpID(int pID) {
+		this.pID = pID;
+	}
+
+	public int getPageNumber() {
+		return pageNumber;
+	}
+
+	public void setPageNumber(int pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+	
+	
 
 }
--- Paging/src/RAM.java
+++ Paging/src/RAM.java
@@ -2,7 +2,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RAM {
+	
+	private List<Process> processList;
+	private Page[] frameValueArray;
+	private int frameCount;
+	
+	public RAM(int framecount) {
+		frameValueArray = new Page[12];
+		processList = new ArrayList<>();
+		this.frameCount = framecount;
+	}
 
+	public List<Process> getProcessList() {
+		return processList;
+	}
+
+	public void setProcessList(List<Process> processList) {
+		this.processList = processList;
+	}
+
+	public Page[] getFrameValueArray() {
+		return frameValueArray;
+	}
+
+	public void setFrameValueArray(Page[] frameValueArray) {
+		this.frameValueArray = frameValueArray;
+	}
+
+	public int getFrameCount() {
+		return frameCount;
+	}
+
+	public void setFrameCount(int frameCount) {
+		this.frameCount = frameCount;
+	}
+	
+	public void addProces(Process process) {
+		processList.add(process);
+	}
+	
+	public void removeProcess(Process process) {
+		processList.remove(process);
+	}
+	
+	public int getProcessListSize() {
+		return processList.size();
+	}
+	
+	public int getFrameValueArrayCount() {
+		return frameValueArray.length;
+	}
+	
+	public void setFrameToNull(int i) {
+		frameValueArray[i] = null;
+	}
+	
+	public void setFrameValueArraySingleEntry(int i, Page page) {
+		frameValueArray[i] = page;
+	}
+	
 
 	
 }
