f91fc4f6fc11a44e351a86ac7aec87eb6b99eb4a
PieterCarlu <pieter.carlu@telenet.be>
2020-04-22 18:21:09 +0200

    alle hw componenten aanwezig en klassen denkk in orde

--- Paging/src/PageTableEntry.java
+++ Paging/src/PageTableEntry.java
@@ -1,5 +1,29 @@
 
 public class PageTableEntry {
+	Page page;
+	TableEntry tableEntry;
+	
+	public PageTableEntry(Page p, TableEntry t) {
+		this.page = p;
+		this.tableEntry = t;
+	}
 
+	public Page getPage() {
+		return page;
+	}
+
+	public void setPage(Page page) {
+		this.page = page;
+	}
+
+	public TableEntry getTableEntry() {
+		return tableEntry;
+	}
+
+	public void setTableEntry(TableEntry tableEntry) {
+		this.tableEntry = tableEntry;
+	}
+	
+	
 
 }
--- Paging/src/TableEntry.java
+++ Paging/src/TableEntry.java
@@ -1,4 +1,73 @@
 
 public class TableEntry {
+	private int pageNumber;
+	private boolean presentBit;
+	private boolean modifyBit;
+	private int lastAccessTime;
+	private int frameNumber; 	//in interval [0,12]
+	
+	public TableEntry() {
+		presentBit = false;
+		modifyBit = false;
+		lastAccessTime = -1;
+		frameNumber = 0;
+	}
+	
+	public TableEntry(int pageNumber, boolean presentBit, boolean modifyBit, int lastAccessTime, int frameNumber) {
+		this.pageNumber = pageNumber;
+		this.modifyBit = modifyBit;
+		this.presentBit = presentBit;
+		this.lastAccessTime = lastAccessTime;
+		this.frameNumber = frameNumber;
+	}
+	
+	public TableEntry(TableEntry t) {
+		this.pageNumber = t.pageNumber;
+		this.modifyBit = t.modifyBit;
+		this.presentBit = t.presentBit;
+		this.lastAccessTime = t.lastAccessTime;
+		this.frameNumber = t.frameNumber;
+	}
 
+	public int getPageNumber() {
+		return pageNumber;
+	}
+
+	public void setPageNumber(int pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+
+	public boolean isPresentBit() {
+		return presentBit;
+	}
+
+	public void setPresentBit(boolean presentBit) {
+		this.presentBit = presentBit;
+	}
+
+	public boolean isModifyBit() {
+		return modifyBit;
+	}
+
+	public void setModifyBit(boolean modifyBit) {
+		this.modifyBit = modifyBit;
+	}
+
+	public int getLastAccessTime() {
+		return lastAccessTime;
+	}
+
+	public void setLastAccessTime(int lastAccessTime) {
+		this.lastAccessTime = lastAccessTime;
+	}
+
+	public int getFrameNumber() {
+		return frameNumber;
+	}
+
+	public void setFrameNumber(int frameNumber) {
+		this.frameNumber = frameNumber;
+	}
+	
+	
 }
